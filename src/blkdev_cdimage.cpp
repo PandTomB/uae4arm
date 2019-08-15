7f3101f72decbf2c71baf6ae690c728dc61b93c3
PandTomB <tborris@web.de>
2019-08-15 16:17:46 +0200

    - Merge with current WinUAE (2019-08-14) - Bugfix JIT (armv7 and aarch64)

--- src/blkdev_cdimage.cpp
+++ src/blkdev_cdimage.cpp
@@ -386,6 +386,7 @@ static bool cdda_play_func2 (struct cdunit *cdu, int *outpos)
 	bool foundsub;
 	int oldtrack = -1;
 	bool restart = false;
+	bool first = true;
 
 	cdu->thread_active = true;
 
@@ -586,6 +587,11 @@ static bool cdda_play_func2 (struct cdunit *cdu, int *outpos)
 					setstate (cdu, AUDIO_STATUS_PLAY_ERROR, -1);
 			  goto end;
 		  }
+
+			if (first) {
+				first = false;
+				setstate(cdu, -3, -1);
+			}
 
 			if (dofinish) {
 				cdda_pos = cdu->cdda_end + 1;
@@ -1590,6 +1596,9 @@ static int parsecue (struct cdunit *cdu, struct zfile *zcue, const TCHAR *img)
 					if (!secoffset) {
 						// secoffset == 0: same file contained also previous track
 						t->offset = fileoffset - pregap * t->size;
+					} else {
+						// pregap was already added, do not add extra silence.
+						t->pregap = 0;
 					}
 					t->address += postgap;
 					if (fnametypeid == AUDENC_PCM && t->handle) {
